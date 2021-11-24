989a41b9f627926a431849a130c04a3d5b37cfbb
3omni <noticiasmundiales99@gmail.com>
2021-11-24 23:27:43 +0000

    Encode and transform by batches

--- model/models.py
+++ model/models.py
@@ -1,34 +1,42 @@
-import os
-import torch
-import numpy as np
+import numba
+import warnings
 from torch import nn
+from tqdm import tqdm
 from tokenizers import BertWordPieceTokenizer
 from transformers import DistilBertModel, DistilBertConfig
 from skorch import NeuralNetClassifier
+warnings.filterwarnings("ignore")
 
 
 class DistilBert(nn.Module):
-    def __init__(self):
+    def __init__(self, bsize = 256):
         super(DistilBert, self).__init__()
-
+        self.bsize = bsize
         self.tokenizer = BertWordPieceTokenizer(os.path.join(os.environ.get("MODEL_PATH"),'distilbert/vocab.txt'), lowercase=False)
-        self.transformer = DistilBertModel.from_pretrained(os.path.join(os.environ.get("MODEL_PATH"),'distilbert/model'))
+        self.transformer = DistilBertModel.from_pretrained(os.path.join(os.environ.get("MODEL_PATH"),'distilbert/model')).cuda()
 
+    # @numba.jit(forceobj=True)
     def encode(self, x, maxlen = 512):
-        all_ids = []   
-        for i in range(0, x.shape[0]):
-            text = x[i,:].astype(str).tolist()
+        all_ids = np.empty((0, maxlen), np.int_)
+        print(f'Tokenizing...')
+        for i in tqdm(range(0, x.shape[0], self.bsize)):
+            text = x[i:i + self.bsize].squeeze().astype(str).tolist()
             # Tokenize current text chunk
             encs = self.tokenizer.encode_batch(text, add_special_tokens=True)
-            # Extending the list = squeezing the list
-            all_ids.extend([enc.ids for enc in encs])
-            # Adding padding
-            all_ids = [i + [tokenizer.token_to_id('[PAD]')] * (maxlen - len(i))
-                                          for i in all_ids]
+            # Extract IDS and add padding
+            padded_encs = [enc.ids[:maxlen] + [self.tokenizer.token_to_id('[PAD]')] * (maxlen - len(enc.ids)) for enc in encs]
+            all_ids = np.vstack((all_ids, np.array(padded_encs)))
         return torch.LongTensor(all_ids)
             
     def forward(self, x):
         x = self.encode(x)
-        o = self.transformer(input_ids=x)
+        x = x.cuda()
+        o = []
+        print('Transforming...')
+        for i in tqdm(range(0, x.shape[0], self.bsize)):
+            o.append(self.transformer(input_ids=x[i:i + self.bsize,:]).last_hidden_state[:,0,:])
+        x = x.cpu()
+        o = torch.cat(o, axis=0)
         # Return only [CLS] transformed token
-        return o.last_hidden_state[:,0,:]
+        return o.detach().cpu()
+    
