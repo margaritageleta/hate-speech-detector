8df77d8af288bea2944337d7daec6c28b06b64b8
3omni <noticiasmundiales99@gmail.com>
2021-11-25 03:19:11 +0000

    Change tokenizer

--- model/models.py
+++ model/models.py
@@ -4,8 +4,8 @@ import warnings
 from torch import nn
 from tqdm import tqdm
 from tokenizers import BertWordPieceTokenizer
-from transformers import DistilBertModel, DistilBertConfig
-#from skorch import NeuralNetClassifier
+from transformers import DistilBertModel, DistilBertConfig, DistilBertTokenizer
+from skorch import NeuralNetClassifier
 warnings.filterwarnings("ignore")
 
 
@@ -13,20 +13,17 @@ class DistilBert(nn.Module):
     def __init__(self, bsize = 256):
         super(DistilBert, self).__init__()
         self.bsize = bsize
-        self.tokenizer = BertWordPieceTokenizer(os.path.join(os.environ.get("MODEL_PATH"),'distilbert/vocab.txt'), lowercase=False)
+        self.tokenizer = DistilBertTokenizer.from_pretrained(os.path.join(os.environ.get("MODEL_PATH"),'distilbert/vocab.txt'), lowercase=False)
         self.transformer = DistilBertModel.from_pretrained(os.path.join(os.environ.get("MODEL_PATH"),'distilbert/model')).cuda()
 
     # @numba.jit(forceobj=True)
     def encode(self, x, maxlen = 512):
         all_ids = np.empty((0, maxlen), np.int_)
         print(f'Tokenizing...')
-        for i in tqdm(range(0, x.shape[0], self.bsize)):
-            text = x[i:i + self.bsize].squeeze().astype(str).tolist()
-            # Tokenize current text chunk
-            encs = self.tokenizer.encode_batch(text, add_special_tokens=True)
-            # Extract IDS and add padding
-            padded_encs = [enc.ids[:maxlen] + [self.tokenizer.token_to_id('[PAD]')] * (maxlen - len(enc.ids)) for enc in encs]
-            all_ids = np.vstack((all_ids, np.array(padded_encs)))
+        encs = distil.tokenizer.batch_encode_plus(x.squeeze(), add_special_tokens=True)['input_ids']
+        # Extract IDS and add padding
+        padded_encs = [enc[:maxlen] + [self.tokenizer.pad_token_id] * (maxlen - len(enc)) for enc in encs]
+        all_ids = np.vstack((all_ids, np.array(padded_encs)))
         return torch.LongTensor(all_ids)
             
     def forward(self, x):
