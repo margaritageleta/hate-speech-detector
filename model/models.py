278d0d3f1023e6c9100f22ef8b75e7de8b6c4d26
3omni <noticiasmundiales99@gmail.com>
2021-11-25 01:28:03 +0000

    gpu cache managemenr

--- model/models.py
+++ model/models.py
@@ -1,10 +1,11 @@
+import gc
 import numba
 import warnings
 from torch import nn
 from tqdm import tqdm
 from tokenizers import BertWordPieceTokenizer
 from transformers import DistilBertModel, DistilBertConfig
-from skorch import NeuralNetClassifier
+#from skorch import NeuralNetClassifier
 warnings.filterwarnings("ignore")
 
 
@@ -30,13 +31,17 @@ class DistilBert(nn.Module):
             
     def forward(self, x):
         x = self.encode(x)
-        x = x.cuda()
         o = []
         print('Transforming...')
         for i in tqdm(range(0, x.shape[0], self.bsize)):
-            o.append(self.transformer(input_ids=x[i:i + self.bsize,:]).last_hidden_state[:,0,:])
-        x = x.cpu()
+            xi = x[i:i + self.bsize,:]
+            xi = xi.cuda()
+            oi = self.transformer(input_ids=xi).last_hidden_state[:,0,:].detach().cpu()
+            o.append(oi)
+            xi = xi.cpu()
+            del xi, oi
+            gc.collect()
+            torch.cuda.empty_cache()
         o = torch.cat(o, axis=0)
         # Return only [CLS] transformed token
         return o.detach().cpu()
-    
